33fb6d280609b85b6a85e7e3e1c73c50b0517608
Ethan Romm <ejromm@gmail.com>
2023-01-15 21:10:45 -0700

    added form  styles and basic js

---
+++ script.js
@@ -0,0 +1,25 @@
+// create popup for adding books to library
+function popUp {
+    
+}
+
+
+
+
+// create library array
+let myLibrary = []; 
+
+// book function
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+// add book function
+function addBookToLibrary {
+    let newBook = new Book(title, author, pages, read); 
+}
