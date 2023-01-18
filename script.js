e8d26cafba6061438969cd9e5e1044db3e184275
Ethan Romm <ejromm@gmail.com>
2023-01-18 11:12:32 -0500

    more js, created cards

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function openPopup() {
     
 
 }
+
 // close button 
 const closeBtn = document.querySelector('.close');
 closeBtn.addEventListener('click', function() {
@@ -15,16 +16,15 @@ closeBtn.addEventListener('click', function() {
     blurContainer.classList.remove('blur')
 })
 
-
-
-
-
-
-
-
-
 // create library array
-let myLibrary = []; 
+let myLibrary = [
+{title: 'Temple of the Golden Pavilion', 
+ author: 'Yukio Mishima', 
+ pages: 239,
+ read: true
+}
+
+]; 
 
 // book function
 class Book {
@@ -35,7 +35,38 @@ class Book {
         this.read = read;
     }
 }
+// variables for inputs
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
+const submitBtn = document.querySelector('#submit');
+
+// function for getting read value
+function getReadValue() {
+    if(readInput.value === 'yes') {
+        return true; 
+    } else {
+        return false;
+    }
+}
+
+
+
 
 // add book function
 function addBookToLibrary() {
+ let title = titleInput.value; 
+ let author = authorInput.value; 
+ let pages = pagesInput.value; 
+ let read = getReadValue(); 
+ let newBook = new Book(title, author, pages, read);
+ myLibrary.push(newBook); 
+
 }
+// submit button event listener  
+submitBtn.addEventListener('click', function() {
+ addBookToLibrary(); 
+}); 
+
+// populate screen with book 
